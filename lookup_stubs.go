7840af533477c7c33e5170fba1cfb891a7f8d8e0
y_uuki <yuki.tsubo@gmail.com>
2015-11-28 04:07:45 +0900

    Fix failure of cross building

--- lookup_stubs.go
+++ lookup_stubs.go
@@ -4,7 +4,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !darwin,!freebsd,!linux
+// +build !cgo,!darwin,!freebsd,!linux
 
 package group
 
